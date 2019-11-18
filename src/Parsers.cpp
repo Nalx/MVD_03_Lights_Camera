36f471bf28f2aa1cfe996ca3e8c9c1a6017359ef
Àlex Cordón Vila <alexcordonvila@gmail.com>
2019-11-18 01:47:57 +0100

    OBJ Parser
    Now we can parse .obj whith tris, quads and tris+quads in it

--- src/Parsers.cpp
+++ src/Parsers.cpp
@@ -3,10 +3,48 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <sstream>
+#include <cstring>
+
+
+
 
 //function that splits a string into an std::vector of (strings)
-void split(std::string to_split, std::string delim, std::vector<std::string>& result) {
-	
+//void split2(std::string to_split, std::string delim, std::vector<std::string>& result) {
+//	// Skip delimiters at beginning.
+//	std::size_t  lastPos = to_split.find_first_not_of(delim, 0);
+//	// Find first "non-delimiter".
+//	std::size_t  pos = to_split.find_first_of(delim, lastPos);
+//	int delim_num = 0;
+//	while (std::string::npos != pos || std::string::npos != lastPos)
+//	{
+//		// Found a token, add it to the vector.
+//		result.push_back(to_split.substr(lastPos, pos - lastPos));
+//		delim_num++;
+//		// Skip delimiters.  Note the "not_of"
+//		lastPos = to_split.find_first_not_of(delim, pos);
+//		// Find next "non-delimiter"
+//		pos = to_split.find_first_of(delim, lastPos);
+//	}
+//	std::cout << result[0] << std::endl;
+//}
+void split(std::string to_split, char delim, std::vector<std::string>& result) {
+	std::replace(to_split.begin(), to_split.end(), delim, ' ');
+	int delim_num = count(to_split.begin(), to_split.end(), delim);
+	std::string frag;
+	for (int i = 0; i < to_split.size();i++) {
+		if (to_split.at(i) != ' ') {
+			frag += to_split.at(i);
+			//std::cout << frag << std::endl;
+		}
+		else if(to_split.at(i) == ' ') {
+			delim_num++;
+			result.push_back(frag);
+			//std::cout << frag <<"\n" << std::endl;
+			frag = ' ';
+		}
+	}
+	result.push_back(frag);
 
 }
 
@@ -16,37 +54,71 @@ bool Parsers::parseOBJ(std::string filename, std::vector<float>& vertices, std::
 	std::vector< lm::vec2 >  unique_uvs;
 	std::vector<std::string> unique_faces, temp_face_index;
 	std::string line;
+	int tris_faces = 0;
+	int quads_faces = 0;
 	std::map<std::string, int> map;
 	int map_index = 0;
-	std::ifstream myfile("data/assets/"+filename);
+	std::ifstream myfile("data/assets/" + filename);
 	//Let’s read this file until the end :
 	if (myfile.is_open()) {
 		while (getline(myfile, line))
 		{
 			std::string text;
+			std::vector< std::string> data;
+			int spaces_no = 0;
+			split(line, ' ', data);
+			
+			std::string first = data[0];
+			int slides = data.size()-1;
+
+			//std::cout << "Talls: " << slides << " First: " << first << std::endl;
+			//std::cout << line << std::endl;
+			
 			// read the first word of the line
-			myfile >> text;
-			if (text == "v") { //Let’s deal with the vertices first 
+			if (first == "v") { //Let’s deal with the vertices first 
 				lm::vec3 vertex;
-				myfile >> vertex.x >> vertex.y >> vertex.z;
+				vertex.x = std::stof(data[1]);
+				vertex.y = std::stof(data[2]);
+				vertex.z = std::stof(data[3]);
 				unique_vertex.push_back(vertex);
 			}
-			else if (text == "vt") { //same thing for the uv's
+			else if (first == "vt") { //same thing for the uv's
 				lm::vec2 uv;
-				myfile >> uv.x >> uv.y;
+				uv.x = std::stof(data[1]);
+				uv.y = std::stof(data[2]);
 				unique_uvs.push_back(uv);
 			}
-			else if (text == "vn") { //same thing for the normals
+			else if (first == "vn") { //same thing for the normals
 				lm::vec3 normal;
-				myfile >> normal.x >> normal.y >> normal.z;
+				normal.x = std::stof(data[1]);
+				normal.y = std::stof(data[2]);
+				normal.z = std::stof(data[3]);
 				unique_normals.push_back(normal);
+
 			}
-			else if (text == "f") { //we use 3 strings for the faces 
+			else if (first == "f" && slides == 3) { //we use 3 strings for the faces
+				tris_faces = tris_faces + 3;
 				std::string fx, fy, fz;
-				myfile >> fx >> fy >> fz;
-				std::vector<std::string> _face{fx,fy,fz};
+				fx = data[1];
+				fy = data[2];
+				fz = data[3];
+				std::vector<std::string> _face{ fx,fy,fz };
 				unique_faces.insert(unique_faces.end(), std::begin(_face), std::end(_face));
 			}
+			else if (first == "f" && slides == 4) {
+				quads_faces = quads_faces + 4;
+				std::string fx, fy, fz,fn;
+				fx = data[1];
+				fy = data[2];
+				fz = data[3];
+				fn = data[4];
+				std::vector<std::string> _face1{fx,fy,fz};
+				std::vector<std::string> _face2{fx,fz,fn};
+				unique_faces.insert(unique_faces.end(), std::begin(_face1), std::end(_face1));
+				unique_faces.insert(unique_faces.end(), std::begin(_face2), std::end(_face2));
+			}
+				
+			
 		}
 		myfile.close();
 	}
@@ -54,24 +126,24 @@ bool Parsers::parseOBJ(std::string filename, std::vector<float>& vertices, std::
 		std::cerr << "Impossible to open the file !\n";
 		return 0;
 	}
-	//Processing the data:
+	int face_num = tris_faces + (quads_faces + quads_faces);
+	//Processing the data: unique_faces.size()
 	for (int i = 0; i < unique_faces.size(); i++) {
 		lm::vec3 vertex, normal;
 		lm::vec2 uv;
 		std::string index = unique_faces.at(i);
+		std::vector< std::string> face_data;
 		int face, vert, u_v, norlm;
 		if (map.count(index) == 0) { //we only add the value if the array index is unique
 			std::pair<std::string, int> faceIndex(index, map_index);
 			temp_face_index.push_back(index);
-			map.insert(faceIndex); 
+			map.insert(faceIndex);
 			indices.push_back(map_index);
 			map_index++;
-			index.erase(std::remove(index.begin(), index.end(), '/'), index.end()); //get rid of "/"
-		    face = atoi(index.c_str()); //string to integer
-			//we've to get each value as separate int
-		    vert = floor((face / 100) % 10);
-		    u_v = floor((face / 10) % 10);
-		    norlm = floor((face % 10));
+			split(index,'/',face_data);
+			vert = stoi(face_data[1]);
+			u_v = stoi(face_data[2]);
+			norlm = stoi(face_data[3]);
 			//we get the correct information according to the int indexes
 			vertex = unique_vertex[vert - 1];
 			uv = unique_uvs[u_v - 1];
@@ -81,13 +153,12 @@ bool Parsers::parseOBJ(std::string filename, std::vector<float>& vertices, std::
 			vertices.insert(vertices.end(), std::begin(_vert), std::end(_vert));
 			uvs.insert(uvs.end(), std::begin(_uvs), std::end(_uvs));
 			normals.insert(normals.end(), std::begin(_nrlm), std::end(_nrlm));
-			
 		}
 		else {  //if the array index is not unique we should also add the value to indices array 
 			indices.push_back(map.at(index));
 		}
 	}
-    return true;
+	return true;
 }
 
 // load uncompressed RGB targa file into an OpenGL texture
