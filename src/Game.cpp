01db4412d4839ce55080cec21be7d6c64085f2ed
Àlex Cordón Vila <alexcordonvila@gmail.com>
2019-11-20 16:35:53 +0100

    Final Push ATOM

--- src/Game.cpp
+++ src/Game.cpp
@@ -17,43 +17,43 @@ Game::Game() {
 void Game::init() {
 
     //******** CREATE AND ADD RESOURCES **********//
-    
+
     //init graphics system
     graphics_system_.init();
-    
+
     //shaders
 	graphics_system_.loadShader("phong", "data/shaders/phong.vert", "data/shaders/phong.frag");
-    
+
     //geometries
     //int teapot_geom_id = graphics_system_.createGeometryFromFile("data/assets/teapot_small.obj");
     int teapot_geom_id = graphics_system_.createPlaneGeometry();
-   
+
     //materials and textures
     int default_mat_id = graphics_system_.createMaterial();
-    graphics_system_.getMaterial(default_mat_id).diffuse_map = Parsers::parseTexture("data/assets/test.tga");
+    graphics_system_.getMaterial(default_mat_id).diffuse_map = Parsers::parseTexture("data/assets/white.tga");
 	graphics_system_.getMaterial(default_mat_id).shader_id = graphics_system_.getShaderProgram("phong");
-    
+
     //******* CREATE ENTITIES AND ADD COMPONENTS *******//
     //don't forget that transforms are automatically created
-    
+
    /* int ent_teapot = ECS.createEntity("Teapot");
     Mesh& tmc = ECS.createComponentForEntity<Mesh>(ent_teapot);
     tmc.geometry = teapot_geom_id;
     tmc.material = default_mat_id;
     ECS.getComponentFromEntity<Transform>(ent_teapot).translate(0.0, -0.5, 0.0);
     ECS.getComponentFromEntity<Transform>(ent_teapot).rotateLocal(-0.7f, lm::vec3(0, 1, 0));*/
-    
+
 	//*****************************************************************CUBE TEST CREATE GEOMETRY FROM OBJ
-	
-	int cube_geom_id = graphics_system_.createGeometryFromFile("cube.obj");
-	//int cube_geom_id = graphics_system_.createGeometryFromFile("teapot_small.obj");
+
+	//int cube_geom_id = graphics_system_.createGeometryFromFile("cube.obj");
+	int cube_geom_id = graphics_system_.createGeometryFromFile("teapot_small.obj");
 
 	int ent_cube = ECS.createEntity("cube");
 	Mesh& cmc = ECS.createComponentForEntity<Mesh>(ent_cube);
 	cmc.geometry = cube_geom_id;
 	cmc.material = default_mat_id;
-	ECS.getComponentFromEntity<Transform>(ent_cube).translate(0.0, -0.3, 0.0);
-	ECS.getComponentFromEntity<Transform>(ent_cube).rotateLocal(-0.9f, lm::vec3(1, 1, 1));
+	ECS.getComponentFromEntity<Transform>(ent_cube).translate(0, -0.6, 1.5);
+	ECS.getComponentFromEntity<Transform>(ent_cube).rotateLocal(-0.4f, lm::vec3(0, 1, 0));
 	//ECS.getComponentFromEntity<Transform>(ent_cube).rotateLocal(+90.0f /180, lm::vec3(1, 1, 0));
 
 
@@ -63,16 +63,16 @@ void Game::init() {
 	// - add camera component and properties (see comments in file)
 
 	// TODO in EntityComponentStore
-	// - note the 'main_camera' property - (index into array of camera components) 
+	// - note the 'main_camera' property - (index into array of camera components)
 
 	//TODO in here in Game::init:
 	// - create entity for light
 	// - add light component
-	// - set transform component properties 
+	// - set transform component properties
 	// - set light component properties
 	int ent_light_1 = ECS.createEntity("Light 1");
 	Light& light_1_comp = ECS.createComponentForEntity<Light>(ent_light_1);
-	light_1_comp.color = lm::vec3(1.0f,1.0f,1.0f);
+	light_1_comp.color = lm::vec3(0.0f,0.0f,0.0f);
 	Transform& light_1_transform = ECS.getComponentFromEntity<Transform>(ent_light_1);
 	light_1_transform.translate(1000.0f,0.0f,1000.0f);
 
@@ -82,7 +82,7 @@ void Game::init() {
 	Transform& light_2_transform = ECS.getComponentFromEntity<Transform>(ent_light_2);
 	light_2_transform.translate(1000.0f, 0.0f, 1000.0f);
 
-	
+
 	int ent_camera = ECS.createEntity("Camera 1");
 	Camera& camera_1_comp = ECS.createComponentForEntity<Camera>(ent_camera);
 	ECS.getComponentFromEntity<Transform>(ent_camera).translate(0, 0, 3.0f);
@@ -98,7 +98,7 @@ void Game::init() {
 	// - set transform component properties
 	// - set camera component properties
 	// - set ECS.main_camera
-	
+
 	//TODO in GraphicsSystem
 	// - replace hard coded view and project matrices with camera component (ECS.main_camera)
 	// - update camera matrices once per frame
@@ -117,4 +117,3 @@ void Game::update(float dt) {
 	graphics_system_.update(dt);
 
 }
-
